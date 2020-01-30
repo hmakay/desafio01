ca373355e2b2330b305d1b355e73c4bcea7dcf43
Hugo Eduardo Fidelis da silva <01360589228@ciccr-am.interno>
2020-01-30 11:29:35 -0400

    commit final

--- index.js
+++ index.js
@@ -7,9 +7,41 @@ app.use(express.json())
 
 
 const projects = []
+let request = 0
+
+/********** funções */
+
+function stringToArray(toString){
+  return  toString.split(',').map( task=> task.trim() );
+}
+
+function countRequest(req,res,next){
+  request++
+  console.log(`Request: ${request}`)
+  return next()
+  
+}
+
+function verifyIdInArray(req, res, next){
+  let project = projects.some( (project) => {
+
+    return (project.id === req.params.id)
+  
+  }) 
+ 
+  if(!project){
+    return res.status(400).json({"error":"this id not found in db"})
+  }
+
+  return next()
+
+}
 
 
-app.post( '/projects', (req,res) => {
+
+
+/*********** rotas */
+app.post( '/projects', countRequest, (req,res) => {
   const {id, title} = req.body
 
     project = {
@@ -27,16 +59,62 @@ app.post( '/projects', (req,res) => {
   return res.json(project)
 })
 
-app.get('/projects',(req,res) => {
+app.post( '/projects/:id/task/', verifyIdInArray, countRequest, (req,res) => {
+  const id = req.params.id
+  const arrayTask = stringToArray(req.body.task)
+
+  const projectArray = projects.filter( p => 
+      p.id === id )
+  console.log(projectArray[0])
+
+  const indexProjects =   projects.indexOf(projectArray[0]) 
+  if(indexProjects >= 0){
+    projects[indexProjects].tasks =  arrayTask
+  }
+
+
+  // let NewProject =  projects.filter( project  => {
+  //   if(project.id===id){
+  //     project.task = arrayTask
+  //   }
+
+  // } )
+
+     
+      
+  return res.json(projects)
+})
+
+app.get('/projects', countRequest, (req,res) => {
   return res.json(projects);
 })
 
-app.put('/projects/:id', (req, res) => {
-  const {id} = res.params  
-  const {title} = res.boby
+app.put('/projects/:id',  verifyIdInArray ,  countRequest,(req, res) => {
 
+
+  const id = req.params.id
+  const {title} = req.body
+  
+  let titles = projects.map( (product) => {
+      if(product.id === id){
+        product.title = title
+        return res.json({"title":product.title })
+        
+      }
+  } )
+
+  app.delete('/projects:id', verifyIdInArray, countRequest, (req, res) => {
+      const id = req.params.id 
+      projects.project[id].tasks.splice();
+
+
+  })
 
   
+
+ 
+ 
+
 })
 
 app.listen(3030);
